a0f4dc08b3c48e01b218c42c5b77bdf11d244433
suyahd8902-star <suyahd8902@gmail.com>
2025-11-29 13:29:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,14 @@
+// config.js - list of candidate public Searx instances.
+// The script will try them in order until one works.
+// You can add or remove instances here anytime.
 const CONFIG = {
-  instanceBase: "https://searx.tiekoetter.com", // change anytime
-  perPage: 10
+  instances: [
+    "https://searx.tiekoetter.com",
+    "https://searx.laquadrature.net",
+    "https://searx.org",            // sometimes present as a redirect
+    "https://searx.be",            // add more if you find them on searx.space
+    "https://searx.space"
+  ],
+  perPage: 10,     // heuristic: how many results per page the instance returns
+  timeoutMs: 8000  // timeout for each instance request
 };
